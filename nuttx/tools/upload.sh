b07b6e145301cc73613cd6e503871519c8487977
Lorenz Meier <lm@inf.ethz.ch>
2014-11-06 15:55:07 +0100

    Default to other board

--- nuttx/tools/upload.sh
+++ nuttx/tools/upload.sh
@@ -2,5 +2,5 @@
 
 BASEDIR=$(dirname $0)
 
-python $BASEDIR/bl_mkfw.py --prototype configs/px4fmu-v2_bare/px4fmu-v2_bare.prototype --image nuttx.bin > nuttx.nxbl
+python $BASEDIR/bl_mkfw.py --prototype configs/px4fmu-v2_upstream/px4fmu-v2_upstream.prototype --image nuttx.bin > nuttx.nxbl
 python $BASEDIR/bl_uploader.py --port /dev/tty.usbmodem1 nuttx.nxbl
